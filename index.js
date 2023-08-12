0c974115a29df0b9f9cabf456e8d1c6ca21c85f6
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-08-12 17:31:11 +0100

    all endpoints implemented from mongodb

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const Joi = require('joi');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/vidly')
+    .then(() => console.log('connected to mongo db'))
+    .catch(err => console.error('Could not connect ', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
