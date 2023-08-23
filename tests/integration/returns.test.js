19bd40f7a33aa17169c42220bbced6a54253d298
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-08-23 16:27:08 +0100

    deploying to heroku

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
-const {User} = require('../../models/user');
-const {Rental} = require('../../models/rental');
+const { User } = require('../../models/user');
+const { Rental } = require('../../models/rental');
 const moment = require('moment');
 const { Movie } = require('../../models/movie');
 
 
 
-describe('/api/returns',()=>{
+describe('/api/returns', () => {
     let server;
     let customerId;
     let movieId;
@@ -16,99 +16,99 @@ describe('/api/returns',()=>{
     let token;
 
 
-    beforeEach(async ()=>{
+    beforeEach(async () => {
         server = require('../../index');
         customerId = new mongoose.Types.ObjectId();
         movieId = new mongoose.Types.ObjectId();
         token = new User().generateAuthToken()
         movie = new Movie({
             _id: movieId,
-            title:'12345',
-            dailyRentalRate:2,
-            genre:'12345',
-            numberInStock:10
+            title: '12345',
+            dailyRentalRate: 2,
+            genre: '12345',
+            numberInStock: 10
         })
 
 
         rental = new Rental({
-            customer:{
-                _id:customerId,
-                name:'12345',
-                phone:'12345'
+            customer: {
+                _id: customerId,
+                name: '12345',
+                phone: '12345'
             },
-            movie:{
-                _id:movieId,
-                title:'12345',
-                dailyRentalRate:2
+            movie: {
+                _id: movieId,
+                title: '12345',
+                dailyRentalRate: 2
 
             }
         });
         await rental.save();
     })
-    afterEach(async ()=>{
+    afterEach(async () => {
         await server.close();
         await Rental.deleteMany({});
         await Movie.deleteMany({});
     })
-    const exec = ()=>{
+    const exec = () => {
         return request(server)
-             .post('/api/returns')
-             .set('x-auth-token',token)
-             .send({
-                customerId,movieId
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({
+                customerId, movieId
             });
     };
 
-    it('should work!',()=>{
+    it('should work!', () => {
         const result = Rental.findById(rental._id);
         expect(result).not.toBeNull();
     });
-    it('should return 401 if client is not loggend in.',async ()=>{
+    it('should return 401 if client is not loggend in.', async () => {
         token = '';
         const res = await exec();
         expect(res.status).toBe(401);
     });
-    it('should return 400 if customerId is not provided.',async ()=>{
+    it('should return 400 if customerId is not provided.', async () => {
         customerId = null;
 
         const res = await exec();
 
         expect(res.status).toBe(400);
     });
-    it('should return 400 if movieId is not provided.',async ()=>{
+    it('should return 400 if movieId is not provided.', async () => {
         movieId = null;
 
         const res = await exec();
 
         expect(res.status).toBe(400);
     });
-    it('should return 404 if no rental found for the customer/movie.',async ()=>{
-       await Rental.deleteMany({});
-       const res = await exec();
+    it('should return 404 if no rental found for the customer/movie.', async () => {
+        await Rental.deleteMany({});
+        const res = await exec();
 
         expect(res.status).toBe(404);
     });
-    it('should return 400 if return is already processed.',async ()=>{
+    it('should return 400 if return is already processed.', async () => {
         rental.dateReturned = new Date();
         await rental.save();
         const res = await exec();
- 
-         expect(res.status).toBe(400);
-     });
-     it('should return 200 if we have a valid requset.',async ()=>{
+
+        expect(res.status).toBe(400);
+    });
+    it('should return 200 if we have a valid requset.', async () => {
         const res = await exec();
- 
-         expect(res.status).toBe(200);
-     });
-     it('should set the return date if input is valid.',async ()=>{
+
+        expect(res.status).toBe(200);
+    });
+    it('should set the return date if input is valid.', async () => {
         const res = await exec();
 
         const rentalInDb = await Rental.findById(rental._id);
         const dif = new Date() - rentalInDb.dateReturned;
-         expect(dif).toBeLessThan(10*1000);
-     });
-     it('should return the dailyRentalRate.',async ()=>{
-        rental.dateOut = moment().add(-7,'days').toDate();
+        expect(dif).toBeLessThan(10 * 1000);
+    });
+    it('should return the dailyRentalRate.', async () => {
+        rental.dateOut = moment().add(-7, 'days').toDate();
         rental.movie.dailyRentalRate = 2
         await rental.save();
 
@@ -116,22 +116,22 @@ describe('/api/returns',()=>{
 
         const rentalInDb = await Rental.findById(rental._id);
 
-         expect(rentalInDb.rentalFee).toBe(14);
-     });
-     it('should increase the movie stock if input is valid.',async ()=>{
+        expect(rentalInDb.rentalFee).toBe(14);
+    });
+    it('should increase the movie stock if input is valid.', async () => {
         const res = await exec();
 
         const movieInDb = await Movie.findById(movieId);
 
         expect(movieInDb.numberInStock).toEqual(movie.numberInStock + 1);
-     });
-     it('should return the rental if input is valid.',async ()=>{
+    });
+    it('should return the rental if input is valid.', async () => {
         const res = await exec();
 
 
         expect(Object.keys(res.body)).toEqual(
-            expect.arrayContaining(['movie','customer','rentalFee','dateReturned','dateOut'])
-            );
+            expect.arrayContaining(['movie', 'customer', 'rentalFee', 'dateReturned', 'dateOut'])
+        );
 
-     });
+    });
 })
